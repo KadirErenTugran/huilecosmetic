c7ef184fa28c8ed89b181228f6d4a36078af472c
Kadir Eren Tuğran <kadirerentugran@hotmail.com>
2024-03-12 01:37:18 +0300

    Added rating section for products

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,16 +1,20 @@
-import { Card } from "react-bootstrap";
+import { Card } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import  Rating from './Rating';
 const Product = ({ product }) => {
   return (
-    <Card className="my-3 p-3 rounded">
+    <Card className='my-3 p-3 rounded '>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
+        <Card.Img src={product.image} variant='top' />
       </Link>
       <Card.Body>
-        <Link to={`/product/${product._id}`}>
-          <Card.Title as="div" />
-          <strong>{product.name}</strong>
+        <Link  to={`/product/${product._id}`}>
+          <Card.Title as='div'  />
+          <strong >{product.name}</strong>
         </Link>
+        <Card.Text as='div'>
+           <Rating value={product.rating} text={`${product.numReviews} Reviews`} />
+        </Card.Text>
         <Card.Text as='h3'>
             ${product.price}
         </Card.Text>
