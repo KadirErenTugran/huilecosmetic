7cb66fb6fc4d8b974d4540c44d2f39767f636a9e
Kadir Eren Tuğran <kadirerentugran@hotmail.com>
2024-03-13 23:23:10 +0300

    model data

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async()=>{
 
     try {
       const   conn= await mongoose.connect(process.env.MONGO_URI)
-      console.log(`MongoDB connected ${conn.connection.host}`);
+      console.log(`MongoDB connected...    ${conn.connection.host}`);
     } catch (error) {
         console.log(`Error: ${error.message}`);
         process.exit(1);
---
+++ backend/models/productModel.js
@@ -0,0 +1,83 @@
+import mongoose, { mongo } from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      name: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    CountInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
