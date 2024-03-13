7891c4b331be9369b3ad7dcff70f06a9a7f01d64
Kadir Eren Tuğran <kadirerentugran@hotmail.com>
2024-03-13 21:24:20 +0300

    Database connect

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose, { mongo } from "mongoose"
+
+
+const connectDB = async()=>{
+
+    try {
+      const   conn= await mongoose.connect(process.env.MONGO_URI)
+      console.log(`MongoDB connected ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+
+}
+
+export default  connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dontenv from  'dotenv';
 dontenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';//ES module .js 
 const port=process.env.PORT ||5000;
+
+connectDB();//Connect to MongDB
+
 const app=express();
 
 app.get('/', (req,res) => {
